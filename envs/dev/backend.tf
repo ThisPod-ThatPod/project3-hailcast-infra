3af38f5c11b778d77b99567526808bd6c7269e7b
yuhyunsang06 <delipure@naver.com>
2026-07-06 17:23:35 +0900

    Feat: backend.tf 파일

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,1 +1,10 @@
 # envs/dev - backend.tf (골격)
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-bucket-9dcbc7fe"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
