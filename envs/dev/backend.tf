f6783edbea94f10890808cefd29c160e6a143628
mshjgr <mshjgr@gmail.com>
2026-07-07 15:01:08 +0900

    Revert "Feat: backend.tf 파일"

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,10 +1,1 @@
 # envs/dev - backend.tf (골격)
-terraform {
-  backend "s3" {
-    bucket         = "hailcast-dev-tfstate-9dcb"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    use_lockfile   = true
-  }
-}
--- envs/dev/versions.tf
+++ envs/dev/versions.tf
@@ -1,10 +1,1 @@
 # envs/dev - versions.tf (골격)
-terraform {
-  required_version = ">=1.15.0"
-  required_providers {
-    aws = {
-        source  = "hashicorp/aws"
-        version = "~> 6.0"
-    }
-  }
-}
