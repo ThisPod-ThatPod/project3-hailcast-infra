2ad7303f958c6168c8052acbc320a5270d5be18b
yuhyunsang06 <delipure@naver.com>
2026-07-10 17:56:20 +0900

    REFAC: output 교정

--- envs/dev/versions.tf
+++ envs/dev/versions.tf
@@ -3,8 +3,8 @@ terraform {
   required_version = ">=1.15.0"
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
--- modules/data/sqs/outputs.tf
+++ modules/data/sqs/outputs.tf
@@ -1,9 +1,9 @@
-output "call_queue_url" {
+output "sqs_queue_url" {
   description = "SQS 콜 데이터 큐의 URL (애플리케이션 컨테이너 및 KEDA Scaler 환경 변수 주입용)"
   value       = aws_sqs_queue.call_queue.id
 }
 
-output "call_queue_arn" {
+output "sqs_queue_arn" {
   description = "SQS 콜 데이터 큐의 ARN (별도의 IAM 모듈이나 타 인프라 파일에서 참조할 때 사용)"
   value       = aws_sqs_queue.call_queue.arn
 }
