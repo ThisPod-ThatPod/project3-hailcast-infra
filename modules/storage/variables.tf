c5de33891de5fe1f4091249a310f6f1ff24529d4
yuhyunsang06 <delipure@naver.com>
2026-07-13 16:35:31 +0900

    Refac: s3.tf, variables.tf 파일 추가

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,1 +1,10 @@
 # storage 모듈 - variables.tf (골격)
+variable "project" {
+  description = "프로젝트 이름 (예: hailcast)"
+  type        = string
+}
+
+variable "environment" {
+  description = "실행 환경 (예: dev)"
+  type        = string
+}
